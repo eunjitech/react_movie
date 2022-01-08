73ff188dbcff76c7c4ad456d73527c78958b1547
이은지/(협력사) 대리/클라우드플랫폼(연) 클라우드플랫폼개발팀(eunjitech.lee@lgepartner.com) <>
2022-01-08 23:25:03 +0900

    20220108 To Do List part2:map&key

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ function App() {
     if (!toDo) {
       return;
     }
-    setToDo('');
+    //현재의 배열과 새로운 요소를 합쳐 새로운 배열을 생성
+    //햔재의 array를 받아와서 새로운 todo의 array로 만듦
     setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
   };
   console.log(toDos);
   return (
@@ -25,6 +27,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, idx) => (
+          <li key={idx}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
