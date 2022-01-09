d0c29ae278de4a3409f4f7525c2cd29ea98fe855
이은지/(협력사) 대리/클라우드플랫폼(연) 클라우드플랫폼개발팀(eunjitech.lee@lgepartner.com) <>
2022-01-09 14:24:09 +0900

    20220109 coin tracker

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (e) => setToDo(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (!toDo) {
-      return;
-    }
-    //현재의 배열과 새로운 요소를 합쳐 새로운 배열을 생성
-    //햔재의 array를 받아와서 새로운 todo의 array로 만듦
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((res) => res.json())
+      .then((json) => setCoins(json));
+    setLoading(false);
+  }, []);
+
   return (
     <div>
-      <h1>My To Dos {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Type here to do"
-          value={toDo}
-          onChange={onChange}
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, idx) => (
-          <li key={idx}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((item) => (
+            <option key={item.id}>
+              {item.name} ({item.symbol}) : {item.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
-
 export default App;
